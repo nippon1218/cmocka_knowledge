071b284b156c7d93b1477dcbc5c6757fcf01f65b
VincentChen <nippon1218@163.com>
2020-05-18 00:22:08 +0800

    new cmocka_test

--- example/basic/cmocka_test.c
+++ example/basic/cmocka_test.c
@@ -5,6 +5,15 @@
 #include <cmocka.h>
 #include <stdbool.h>
 
+typedef struct 
+{
+    int x;
+    int y;
+}test_segv_t;
+
+static test_segv_t segv;
+
+
 static bool is_single(int num)
 {
     return (num % 2 == 0) ? false : true ;
@@ -17,7 +26,8 @@ static int sum(int a, int b)
 
 static void assert_test(void **state) {
     const unsigned long test_sets[3]={1, 3, 5};
-
+    (void) state;
+//    fail_msg("assert_test");
     assert_int_equal(3, 3);
     assert_int_not_equal(2, 4);
     assert_float_equal(2.0, 2.1, 0.4);
@@ -37,12 +47,62 @@ static void assert_test(void **state) {
     assert_return_code(sum(2, 3), 0);
     assert_string_equal("hello", "hello");
     assert_string_not_equal("hello", "world");
+}
 
+/*
+ * mockfunction 1
+ */
+static int mock_function(void)
+{
+  return (int) mock();
+}
+
+static void mock_function_test(void **state)
+{
+    (void) state;
+    will_return(mock_function, 12);
+    assert_int_equal(mock_function(), 12);
+}
+
+/*
+ * mockfunction 2
+ */
+static int mock_function2(char **out)
+{
+    *out = (char *) mock();
+    return (int) mock();
+}
+
+static void mock_function_test2(void **state)
+{
+    char *out = NULL;
+    const char * const hel = "hello world";
+
+    will_return(mock_function2, hel);
+    will_return(mock_function2, 42);
+
+    assert_int_equal(mock_function2(&out), 42);
+    assert_ptr_equal(out, hel);
+}
+
+/*
+ * except test
+ */
+static void test_segfault_recovery(void **state)
+{
+//    test_segv_t *seg = NULL; //if use this will fail
+    test_segv_t *seg = &segv;
+    
+    seg->x = 1;
+    seg->y = 2;
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(assert_test),
+        cmocka_unit_test(mock_function_test),
+        cmocka_unit_test(mock_function_test2),
+        cmocka_unit_test(test_segfault_recovery)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
---
+++ example/basic/cmocka_test.mk
@@ -0,0 +1,1 @@
+CSRCS += cmocka_test.c
